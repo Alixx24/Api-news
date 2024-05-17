37fb881d391dbb230f721936cc4c40e855030f25
Alixx24 <154596621+Alixx24@users.noreply.github.com>
2024-05-17 19:46:33 +0330

    Update LastNewsguardian.php

--- app/Livewire/Home/LastNewsguardian.php
+++ app/Livewire/Home/LastNewsguardian.php
@@ -12,7 +12,7 @@ class LastNewsguardian extends Component
     public $latest_news;
     public function mount()
     {
-        $apiKey = 'c6397f0b-bd63-40b2-a4fe-8610b24461c8';
+        $apiKey = '***';
         $url = 'https://content.guardianapis.com/search?api-key=' . $apiKey . '&order-by=newest&page-size=5';
         $response = Http::get($url);
         if ($response->successful()) {
